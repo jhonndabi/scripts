1dc2610de8b01fb8fa5b46e11cfc63561bb4d9dc
Jonadabe S. Nascimento <jhonndabi.s.n@hotmail.com>
2018-06-20 16:36:31 -0300

    add clean memory script

---
+++ clean-memory.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo 3 > /proc/sys/vm/drop_caches > /dev/null
+sysctl -w vm.drop_caches=3 > /dev/null
+
+swapoff -a > /dev/null
+echo "desligando memória swap" > /dev/null
+
+echo 3 > /proc/sys/vm/drop_caches > /dev/null
+sysctl -w vm.drop_caches=3 > /dev/null
+
+swapon -a  > /dev/null
+echo "reativando memória swap" > /dev/null
